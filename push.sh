2ddd32ca9d4d5cd537c3756ca91a4b0eb242dd91
wizlijun <me@bruce.com>
2025-05-21 16:19:15 +0800

    2025-05-21 16:19:15

--- push.sh
+++ push.sh
@@ -2,6 +2,8 @@
 
 python3 genindex.py ~/git/xiaobu/ai https://www.xiaobu.net/ai/
 
+export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;
+
 git add .
 
 git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
