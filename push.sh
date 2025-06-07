7b331a59821c74af0c27af771c47bb36dd24a8c9
Bruce <lijun@wiz.cn>
2025-06-07 13:38:47 +0800

    2025-06-07 13:38:47

--- push.sh
+++ push.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python3 -m venv venv
-source venv/bin/activate
+#python3 -m venv venv
+#source venv/bin/activate
 
-python genindex.py ~/git/xiaobu/ai https://www.xiaobu.net/ai/
+python3 genindex.py ~/git/xiaobu/ai https://www.xiaobu.net/ai/
 
 export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;
 
