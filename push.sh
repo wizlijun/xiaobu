99694d36d4ab255f97c09f1c3aedffd88671912a
wizlijun <me@bruce.com>
2025-09-07 10:54:39 +0800

    2025-09-07 10:54:39

--- push.sh
+++ push.sh
@@ -3,8 +3,11 @@
 python3 -m venv venv
 source venv/bin/activate
 
+python3 parse_tags.py
+
 python3 genindex.py ~/git/xiaobu/ai https://www.laobu.net/ai/
 python3 gensitemap.py -u https://www.laobu.net
+
 
 export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;
 
